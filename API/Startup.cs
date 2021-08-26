b892fd61e3c9e0e47db9e3abffeed442978a8bd2
Bagheri_H <Bagheri_H@IRANCODE-3162>
2021-08-26 16:03:50 +0330

    Learn First Api

--- API/Startup.cs
+++ API/Startup.cs
@@ -39,6 +39,7 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+            //services.addCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
